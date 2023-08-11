e470db4dacd252dcecd5bd2a7067eb28a4e548da
Anazz24 <analauramoraes87@gmail.com>
2023-08-11 12:26:50 -0300

    mais

---
+++ src/pages/Carrinho/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Image} from 'react-native';
+
+import topo from "../../../assets/topo.png";
+
+export default function Carrinho(){
+
+    return <Image source={topo}/>
+}
